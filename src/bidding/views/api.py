f999788f5cfd1add54c3aa49e1746cb604dcbf89
Sharma <shanshul@amazon.com>
2015-04-25 19:30:26 +0530

    Initial Campaign post api

--- src/bidding/views/api.py
+++ src/bidding/views/api.py
@@ -4,8 +4,9 @@ from . import api_blueprint as app
 from flask.ext.classy import FlaskView
 from bidding import models as km
 import json
-from flask import Response, Request
+from flask import Response, request
 from bson import json_util
+from datetime import datetime
 
 class Campaign(FlaskView):
     def index(self):
@@ -27,7 +28,32 @@ class Campaign(FlaskView):
             return Response(json.dumps({'details':'Exception: %s'%repr(e)}), status=403, content_type="application/json")
 
     def post(self):
-        return NotImplemented
+        data = request.form
+        if not data:
+            data = request.json
+        campaign = km.Campaign()
+        keys = data.keys()
+        if 'city' in keys:
+            campaign.city = data['city']
+        if 'officeLocation' in keys:
+            campaign.officeLocation = data['officeLocation']
+        if 'localityPref' in keys:
+            campaign.localityPref = data['localityPref']
+        if 'poi' in keys:
+            campaign.poi = data['poi']
+        if 'livingCost' in keys:
+            try:
+                campaign.livingCost = int(data['livingCost'])
+            except Exception,e :
+                return Response(json.dumps({'details':'Exception: %s'%repr(e)}), status=403, content_type="application/json")
+        if 'priorities' in keys:
+            campaign.priorities = data['priorities']
+        campaign.added_on = datetime.now()
+        try:
+            campaign.save()
+            return Response(json.dumps(campaign.to_json(), default=json_util.default), status=200, content_type="application/json")
+        except Exception,e :
+            return Response(json.dumps({'details':'Exception: %s'%repr(e)}), status=403, content_type="application/json")
 
 
 Campaign.register(app)
